cb83ed0e9eb650a79e5659dc862109920736cde9
Aiden Howard <achoward@usc.edu>
2025-03-06 18:06:48 -0800

    to github

--- llrec.cpp
+++ llrec.cpp
@@ -1,7 +1,30 @@
 #include "llrec.h"
+#include "iostream"
+
 
 //*********************************************
 // Provide your implementation of llpivot below
 //*********************************************
 
+void llpivot(Node *&head, Node *&smaller, Node *&larger, int pivot){
 
+    if(head == NULL){
+        smaller = NULL;
+        larger = NULL;
+
+        return;
+    }
+
+    if(head->val > pivot){
+        larger = head;
+        llpivot(head->next, smaller, larger->next, pivot);
+    }
+
+    if(head->val < pivot){
+        smaller = head;
+        llpivot(head->next, smaller->next, larger, pivot);
+    }
+
+
+
+}
